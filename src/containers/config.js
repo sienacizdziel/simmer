b5639e614ab65e0f7dc84a6ba661919e2bd63c2d
Andy Wu <andy.wu@yale.edu>
2021-01-16 15:24:40 -0800

    added firebase to home js

---
+++ src/containers/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCjO5G0GcNPneTrBdracN1X6LSBDFAU7es",
+    authDomain: "simmer-hackthenorth.firebaseapp.com",
+    databaseURL: "https://simmer-hackthenorth-default-rtdb.firebaseio.com",
+    projectId: "simmer-hackthenorth",
+    storageBucket: "simmer-hackthenorth.appspot.com",
+    messagingSenderId: "980809921947",
+    appId: "1:980809921947:web:bfe55452134d18e0a39d3e",
+    measurementId: "G-D0JRLHDVNQ"
+  };
