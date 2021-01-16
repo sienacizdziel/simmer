66590d5e3b5cdb42b94f6bec1b268725b5465ada
Andy Wu <andy.wu@yale.edu>
2021-01-16 11:17:54 -0800

    added in firebase backend

--- src/backend/firebase/index.js
+++ src/backend/firebase/index.js
@@ -1,44 +1,26 @@
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    if (user != null) {
-      document.getElementById("user_para").innerHTML =
-        "Welcome user:" + email.id;
-    }
-  } else {
-  }
-});
 function submit() {
   var recipe_name = document.getElementById("recipe_field").value;
   var ingredient_1 = document.getElementById("ingredient_1_field").value;
-//   window.alert("recipe name is " + recipe_name);
-//   window.alert("ingredient 1 is" + ingredient_1);
-//   firebase
-//     .auth()
-//     // .createUserWithEmailAndPassword(userEmail, userPass)
-//     .catch(function (error) {
-//       // Handle Errors here.
-//       var errorCode = error.code;
-//       var errorMessage = error.message;
-//       // ...
-//       windows.alert("error: " + errorMessage);
-//     });
-
+  var cuisine = document.getElementById("cuisine_field").value;
   window.alert("recipe name is " + recipe_name);
   window.alert("ingredient 1 is" + ingredient_1);
-  db.collection("Recipes")
-    .doc(recipe_name)
-    .set({
-      ingredient_1: ingredient_1
-    })
-    .then(function () {
-      console.log("added in recipe name and ingredient");
-    })
-    .catch(function (error) {
-      console.log("got an error", error);
-    });
-    window.location.replace("index.html");
+  window.alert("cuisine is" + cuisine);
+  
+//   db.collection("Recipe")
+//     .doc(recipe_name)
+//     .set({
+//       recipeIngredient: ingredient_1,
+//       recipeCuisine: cuisine
+//     })
+//     .then(function () {
+//       console.log("added in recipe");
+//     })
+//     .catch(function (error) {
+//       console.log("got an error", error);
+//     });
+    window.location.replace("test.html");
 
 }
