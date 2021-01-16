1bcc0e2dc17f07028152771875333acb00d72eac
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2021-01-16 15:33:48 -0500

    c3

--- src/containers/FindRecipe.js
+++ src/containers/FindRecipe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchBar from './SearchBar';
 import RecipeList from './RecipeList';
-import RecipeItems from "./RecipeItems"
+/*import RecipeItems from "./RecipeItems"*/
 import "./FindRecipe.css";
 
 const FindRecipe = (props) => {
