e21e10116dd3763ea66fe078a89aea5c8cb3a743
Siena <siena.cizdziel@yale.edu>
2021-01-15 22:41:23 -0600

    nav links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Routes from './Routes';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <ul>
+          <Link to="/">Home</Link>
+          <Link to="/recipe">Recipe Page</Link>
+          <Link to="/addrecipe">Add Recipe Page</Link>
+      </ul>
       <Routes />
     </div>
   );
