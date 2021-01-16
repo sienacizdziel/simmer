ba653a00e278b45ae45c652e1385131f5e07bd5e
Andy Wu <andy.wu@yale.edu>
2021-01-15 22:52:26 -0800

    almost finished fire base submit recipes, but small bug not sure what

--- src/backend/firebase/index.js
+++ src/backend/firebase/index.js
@@ -10,57 +10,35 @@ firebase.auth().onAuthStateChanged(function (user) {
   } else {
   }
 });
-function signup() {
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  var user_Name = document.getElementById("name_field").value;
-  var user_Business = document.getElementById("business_field").value;
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(userEmail, userPass)
-    .catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-      windows.alert("error: " + errorMessage);
-    });
+function submit() {
+  var recipe_name = document.getElementById("recipe_field").value;
+  var ingredient_1 = document.getElementById("ingredient_1_field").value;
+//   window.alert("recipe name is " + recipe_name);
+//   window.alert("ingredient 1 is" + ingredient_1);
+//   firebase
+//     .auth()
+//     // .createUserWithEmailAndPassword(userEmail, userPass)
+//     .catch(function (error) {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // ...
+//       windows.alert("error: " + errorMessage);
+//     });
 
-  window.alert("username is " + user_Name);
-  window.alert("userBusiness is" + user_Business);
-  db.collection("users")
-    .doc(userEmail)
+  window.alert("recipe name is " + recipe_name);
+  window.alert("ingredient 1 is" + ingredient_1);
+  db.collection("Recipes")
+    .doc(recipe_name)
     .set({
-      userName: user_Name,
-      userBusiness: user_Business,
+      ingredient_1: ingredient_1
     })
     .then(function () {
-      console.log("added in name and business name");
+      console.log("added in recipe name and ingredient");
     })
     .catch(function (error) {
       console.log("got an error", error);
     });
-    window.location.replace("logout.html");
-}
-function login() {
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  window.alert("username is " + user_Name);
-  window.alert("userBusiness is" + user_Business);
+    window.location.replace("index.html");
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(userEmail, userPass)
-    .catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      windows.alert("error: " + errorMessage);
-      // ...
-    });
-  window.location.replace("logout.html");
-}
-function logout() {
-  firebase.auth().signOut();
-  window.location.replace("login.html");
 }
