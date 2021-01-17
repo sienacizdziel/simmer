dbea16f6e0886eac62b6e33c42df443b23a09a32
Siena <siena.cizdziel@yale.edu>
2021-01-16 22:22:16 -0600

    centering simmer

---
+++ src/containers/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+  <>
+    <h1 className="not-found">sorry, page not found. please return to homepage.</h1>
+  </>
+  );
+}
