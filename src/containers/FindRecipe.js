c5d270b68d82363b6f1f132d67c91760c765e7c1
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2021-01-16 23:01:06 -0500

    c9

--- src/containers/FindRecipe.js
+++ src/containers/FindRecipe.js
@@ -5,15 +5,20 @@ export default function FindRecipe() {
   return (
   <>
     <div className="searchbar">
-         <input type="text" placeholder="Search for..." /><br></br>
+         <input type="text" placeholder="Search for..." />
      </div>
+     <br></br>
      <div className="label">
-         <h2>Select one of today's popular meals!</h2>
+         <h2>Select one of today's popular dishes!</h2>
      </div>
-    <div className="images">
-        <img id="leftimg" src="https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"/>
-        <img id="middleimg" src="https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"/>
-        <img id="rightimg" src="https://images.unsplash.com/photo-1529042410759-befb1204b468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80"/>
+     <br></br>
+    <div className="container">
+        <img id="leftimg" className="images left col-4" src="https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"/>
+          <div className="text1 text">Soup</div>
+        <img id="middleimg" className="images middle col-4" src="https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"/>
+          <div className="text2 text">Breadsoup</div>
+        <img id="rightimg" className="images right col-4" src="https://images.unsplash.com/photo-1529042410759-befb1204b468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80"/>
+        <div className="text3 text">Meatballs</div>
     </div>
   </>
   );
