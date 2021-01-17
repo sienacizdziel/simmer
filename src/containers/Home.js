f7ef834b04ca0f9ab654385376ad7ee25c7f9984
Andy Wu <andy.wu@yale.edu>
2021-01-16 19:03:09 -0800

    added in table to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     render(){
       return (      
             <>
-              <div className="MainDiv">
+        <div className="MainDiv">
         <div class="jumbotron text-center bg-sky">
             <h3>Firebase Data</h3>
         </div>
@@ -58,9 +58,40 @@ class App extends React.Component {
               </tbody>
               
            </table>
-            
        </div>
+       <div class="jumbotron text-center bg-sky">
+            <h3>User List</h3>
+        </div>
+
+       <div className="container">
+            <table id="example" class="display table">
+              <thead class="thead-dark">
+                  <tr>
+                      <th>Friend</th>
+                      <th>Favorite Cuisine</th>
+                      <th>Cooking Tonight</th>
+                  </tr>
+              </thead>
+              <tbody>
+              {this.state.recipelist.map(data => {
+                  
+                  return (
+                      <tr>     
+                      <td>{data.cuisine}</td>
+                      <td>{data.ingredient1}</td>
+                      </tr>
+                      
+                  );
+                 
+                  })}         
+              </tbody>
+              
+           </table>
+       </div>
+
       </div>
+
+      
           
             <div id="charts">
               <h1>Top Charts</h1>
@@ -145,115 +176,3 @@ class App extends React.Component {
 
 
 
-// export default function Home() {
-//   return (      
-//     <>
-//     <div className="MainDiv">
-//       <div class="jumbotron text-center bg-sky">
-//           <h3>Firebase Data goes here</h3>
-//       </div>
-//       <table id="example" class="display table">
-//             <thead class="thead-dark">
-//                 <tr>
-//                     <th>FirstName</th>
-//                     <th>Lastname</th>
-//                     <th>Email</th>
-//                     <th>Mobile</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//               <h1>{recipelist[0]}</h1>
-//             {/* {this.state.recipelist.map(data => {
-//                 return (
-//                   <h1>data.cuisine</h1>
-//                     // <tr>     
-
-//                     // <td>{data.firstName}</td>
-//                     // <td>{data.lastName}</td>
-//                     // <td>{data.email}</td>
-//                     // <td>{data.mobileNumber}</td>
-//                     // </tr>
-//                 );
-//                 })} */}
-//             </tbody>
-//       </table>
-//     </div>
-
-  
-  
-//     <div id="charts">
-//       <h1>Top Charts</h1>
-//       <div>
-//         <table className="user-chart">
-//           <tr className="header">
-//             <td>Cook</td>
-//             <td>Recipes Completed</td>
-//           </tr>
-//           <tr>
-//             <td>iluvchocolate42</td>
-//             <td>3001</td>
-//           </tr>
-//           <tr>
-//             <td>applesauce</td>
-//             <td>324</td>
-//           </tr>
-//           <tr>
-//             <td>billy joel</td>
-//             <td>5</td>
-//           </tr>
-//         </table>
-//       </div>
-//       <div className="winner-container">
-//         <button className="user winner">user profile</button>
-//         <button className="user winner">food image</button>
-//       </div>
-//       <div>
-//         <table className="food-chart">
-//           <tr className="header">
-//             <td>Recipe</td>
-//             <td># of Times Made</td>
-//           </tr>
-//           <tr>
-//             <td>double choco chip cookies</td>
-//             <td>50000</td>
-//           </tr>
-//           <tr>
-//             <td>margherita pizza</td>
-//             <td>2500</td>
-//           </tr>
-//           <tr>
-//             <td>pad thai</td>
-//             <td>100</td>
-//           </tr>
-//         </table>
-//       </div>
-//     </div>
-
-//     <div id="social">
-
-//       <h1>Social</h1>
-//       <h2>see what your friends are cooking!</h2>
-//       <div className="friend-container">
-//         <button className="user friends left">friend profile</button>
-//         <h2 className="friend-recipe">CAESAR SALAD</h2>
-//         <h3>michelle:)</h3>
-//       </div>
-//       <div className="friend-container">
-//         <button className="user friends right">friend profile</button>
-//         <h2 className="friend-recipe">HOT BUFFALO WINGS</h2>
-//         <h3>siena123</h3>
-//       </div>
-//       <div className="friend-container">
-//         <button className="user friends left">friend profile</button>
-//         <h2 className="friend-recipe">TACOS</h2>
-//         <h3>wooandywoo</h3>
-//       </div>
-//       <div className="friend-container">
-//         <button className="user friends right">friend profile</button>
-//         <h2 className="friend-recipe">RAMEN</h2>
-//         <h3>vyomesh!</h3>
-//       </div>
-//     </div>
-//   </>
-//   );
-// }
