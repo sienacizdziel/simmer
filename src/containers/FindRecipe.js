5721406fa30af1ec044d11875f16664b7ab95a76
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2021-01-16 21:25:36 -0500

    c8

--- src/containers/FindRecipe.js
+++ src/containers/FindRecipe.js
@@ -5,10 +5,10 @@ export default function FindRecipe() {
   return (
   <>
     <div className="searchbar">
-         <input type="text" placeholder="Search for..." />
+         <input type="text" placeholder="Search for..." /><br></br>
      </div>
      <div className="label">
-         <h2>Select one of today's popular dishes!</h2>
+         <h2>Select one of today's popular meals!</h2>
      </div>
     <div className="images">
         <img id="leftimg" src="https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"/>
