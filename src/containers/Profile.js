58772c8e924071aa6bf8982e0e7d3bb55d6d14d2
Michelle Lin <michelle.xlin83@gmail.com>
2021-01-16 23:46:03 -0500

    updated user profile page

---
+++ src/containers/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { IoBookmarks } from 'react-icons/io5';
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+  <div id= "wrapper">
+      <div class="clearer"></div>
+    </div>
+  );
+}
